4039ac06c9e6cdae47e0b252dafc4f327522b9ef
Kiattiphum <83391695+KiattiphumSuw@users.noreply.github.com>
2024-02-18 10:03:03 +0000

    code day 2

--- 00-bootcamp-project/load_data_to_bigquery.py
+++ 00-bootcamp-project/load_data_to_bigquery.py
@@ -11,16 +11,19 @@ DATA_FOLDER = "data"
 # จะทำให้เราไม่ต้อง Hardcode Path ของไฟล์ไว้ในโค้ดของเรา
 # keyfile = os.environ.get("KEYFILE_PATH")
 
-keyfile = "YOUR_KEYFILE_PATH"
+keyfile = "durable-matter-414109-deb3-load-files-to-bigquery-722eedcb3063.json"
 service_account_info = json.load(open(keyfile))
 credentials = service_account.Credentials.from_service_account_info(service_account_info)
-project_id = "dataengineercafe"
+project_id = "durable-matter-414109"
 client = bigquery.Client(
     project=project_id,
     credentials=credentials,
 )
 
-# Addressess
+# YOUR CODE HERE
+# unpartition tables are order_items products promos
+files_list = ['addresses', 'order_items','products','promos']
+
 job_config = bigquery.LoadJobConfig(
     skip_leading_rows=1,
     write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
@@ -28,19 +31,21 @@ job_config = bigquery.LoadJobConfig(
     autodetect=True,
 )
 
-data = "addresses"
-file_path = f"{DATA_FOLDER}/{data}.csv"
-with open(file_path, "rb") as f:
-    table_id = f"{project_id}.deb_bootcamp.{data}"
-    job = client.load_table_from_file(f, table_id, job_config=job_config)
-    job.result()
+for data in files_list:
+    file_path = f"{DATA_FOLDER}/{data}.csv"
+    with open(file_path, "rb") as f:
+        table_id = f"{project_id}.deb_bootcamp.{data}"
+        job = client.load_table_from_file(f, table_id, job_config=job_config)
+        job.result()
 
-table = client.get_table(table_id)
-print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+    table = client.get_table(table_id)
+    print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
 
-# ----------
 
-# Events
+# partition tables are orders users
+# In the formatting, file name : date
+partition_files_dict = {'events':'2021-02-10', 'orders':"2021-02-10", 'users':"2020-10-23"}
+
 job_config = bigquery.LoadJobConfig(
     skip_leading_rows=1,
     write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
@@ -52,17 +57,13 @@ job_config = bigquery.LoadJobConfig(
     ),
 )
 
-dt = "2021-02-10"
-partition = dt.replace("-", "")
-data = "events"
-file_path = f"{DATA_FOLDER}/{data}.csv"
-with open(file_path, "rb") as f:
-    table_id = f"{project_id}.deb_bootcamp.{data}${partition}"
-    job = client.load_table_from_file(f, table_id, job_config=job_config)
-    job.result()
+for data, dt in partition_files_dict.items():
+    partition = dt.replace("-", "")
+    file_path = f"{DATA_FOLDER}/{data}.csv"
+    with open(file_path, "rb") as f:
+        table_id = f"{project_id}.deb_bootcamp.{data}${partition}"
+        job = client.load_table_from_file(f, table_id, job_config=job_config)
+        job.result()
 
-table = client.get_table(table_id)
-print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
-
-# ถึงตรงนี้เราโหลดข้อมูลไปแล้ว 2 ชุด ยังเหลืออีก 5 ชุดที่ต้องโหลดเพิ่ม
-# YOUR CODE HERE
+    table = client.get_table(table_id)
+    print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
